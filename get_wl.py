c01e13a542f4f6d7e4b608dc4ff8a027e59eed69
abhinavsaurabh <abhinavsaurabh2@gmail.com>
2018-09-23 11:01:10 +0530

    Add files via upload

---
+++ get_wl.py
@@ -0,0 +1,24 @@
+#groups must be submitted to the simulator in order
+
+A = ['Russia', 'Saudi Arabia', 'Egypt', 'Uruguay']
+B = ['Portugal', 'Spain', 'Morocco', 'Iran']
+C = ['France', 'Australia', 'Peru', 'Denmark']
+D = ['Argentina', 'Iceland', 'Croatia', 'Nigeria']
+E = ['Brazil', 'Switzerland', 'Costa Rica', 'Serbia']
+F = ['Germany', 'Mexico', 'Sweden', 'South Korea']
+G = ['Belgium', 'Panama', 'Tunisia', 'England']
+H = ['Poland', 'Senegal', 'Colombia', 'Japan']
+
+# winners_order = [A C E G B D F H]
+# runners_order = [B D F H A C E G] #these orderings will be used in tournament.py
+
+groups = [A,B,C,D,E,F,G,H] #TODO: convert names to indices
+
+print("Group A -",A)
+print("Group B -",B)
+print("Group C -",C)
+print("Group D -",D)
+print("Group E -",E)
+print("Group F -",F)
+print("Group G -",G)
+print("Group H -",H)
